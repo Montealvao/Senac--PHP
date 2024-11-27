a9a26e6a12df3d952a1655afb7e4cd5dfe68fd72
Guilherme Montealvão <guilhermehmc06@gmail.com>
2024-11-27 17:12:00 -0400

    ref:

--- Exercise 4/index.php
+++ Exercise 4/index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,10 +10,36 @@
 <body>
     <form method="post">
         <input type="text" name="input" placeholder="teste">
-        <button type="submit" name="add">Adicionar  button>
+        <button type="submit" name="add">Adicionar</button>
         <button type="submit" name="remove">Remover</button>
-
+        <button type="submit" name="view">Ver lista</button>
+        <button type="submit" name="conclude">Concluir</button>
     </form>    
+
+    <?php
+    require "./task-manager.php";
+    if($_SERVER['REQUEST_METHOD'] ==='POST'){
+        if(isset($_POST['input'])){
+            $name = $_POST['input'];
+            if(isset($_POST["add"])){
+                echo add_task($name);
+            }
+            
+            else if(isset($_POST["remove"])){
+                echo remove_task($name);
+
+            }
+
+            else if(isset($_POST["view"])){
+                echo tasks_array();
+            }
+            
+            // else if($_POST["conclude"]){
+            //     echo task_conclude_marker();
+            // }
+        }
+    }
+    ?>
 
 </body>
 </html>
--- Exercise 4/task-manager.php
+++ Exercise 4/task-manager.php
@@ -1,23 +1,34 @@
 <?php
 
-$tasks = [];
+session_start();
+if(!isset($_SESSION['tasks'])){
+    $_SESSION['tasks'] = [];
+}
 
 function add_task($name){
-    array_push($tasks, $name);
-    echo "Adicionado com sucesso";
+    $P = "Pendente";
+    array_push($_SESSION['tasks'], "$name - $P");
 }
 
 function remove_task($name){
-    unset($tasks[$name]);
-    echo "Removido com sucesso";
+    foreach($_SESSION['tasks'] as $key=>$array){
+        if($array == $name){
+            unset($_SESSION['tasks'][$key]);
+        }
+    }
 }
 
 function tasks_array(){
-
-
+    foreach($_SESSION['tasks'] as $array){
+        echo "<li>$array</li>";
+    }
 }
 
-function task_conclude_marker(){
-    
+function task_conclude_marker($name){
+    foreach($_SESSION["tasks"] as $array){
+      if($array ==$name) {
+        
+      }
+    }
 }
 ?>
